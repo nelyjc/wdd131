d177b00780049a16ad8642f37c480f1b08eb94ed
nelyjc <nely.jazmin@outlook.com>
2025-05-20 13:58:17 -0600

    1

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,24 @@
-const input document.querySelector('#favchap');
-const button document.querySelector('button');
-const list document.querySelector('#bom');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = 'X';
-li.append(deleteButton);
-list.append(li);
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#bom');
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        input.value = '';
+        input.focus();
+    }
+})
