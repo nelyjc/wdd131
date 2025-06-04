6785f7fa5d3765d49e09df9451f6600c16dce3ad
nelyjc <nely.jazmin@outlook.com>
2025-06-04 14:31:16 -0600

    123

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,13 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Set the last modified date
 document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+
+const menuButton = document.querySelector("#menu");
+const nav = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+  nav.classList.toggle("open");
+
+  const expanded = menuButton.getAttribute("aria-expanded") === "true" || false;
+  menuButton.setAttribute("aria-expanded", !expanded);
+});
