7aec6439a891c38e56566f661e891a5302c0cc9e
nelyjc <nely.jazmin@outlook.com>
2025-06-04 13:56:27 -0600

    1

---
+++ scripts/lazyload.js
@@ -0,0 +1,5 @@
+// Set the current year in the footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Set the last modified date in the footer
+document.getElementById("lastModified").textContent = document.lastModified;
