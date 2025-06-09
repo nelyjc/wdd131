a205b4139ceb4400025382c02a68b7294071cb03
nelyjc <nely.jazmin@outlook.com>
2025-06-09 12:32:30 -0600

    picusm

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,8 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Set the last modified date
 document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+
+const yearEl = document.getElementById("year");
+if (yearEl) {
+  yearEl.textContent = new Date().getFullYear();
+}
