80bdaef61d32eef05e22b34eb17b5c6ac4f52147
nelyjc <nely.jazmin@outlook.com>
2025-06-09 13:23:20 -0600

    final

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,5 @@
 // Set the current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("year").textContent = new Date().getFullYear();
 
 // Set the last modified date
 document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
-
-const yearEl = document.getElementById("year");
-if (yearEl) {
-  yearEl.textContent = new Date().getFullYear();
-}
